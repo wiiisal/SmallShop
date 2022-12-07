e0c59419a1fed03af94311d073785082a8e1c808
Wiiisal <wissalbensai011@gmail.com>
2022-12-07 20:52:14 +0100

    nav

---
+++ persohype/src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav class="navbar bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Offcanvas navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+    </div>
+  </div>
+</nav>
+  )
+}
